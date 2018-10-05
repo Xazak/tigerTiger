9d9a75f3ed009a5a7d2c139d75804c84ceb17c17
xazak <xazak47@gmail.com>
2018-10-05 13:45:30 -0700

    added some more of the important libraries, started makefile and devNotes, removed Windows libraries for now

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "libtcod.hpp"
+int main() {
+TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
+while ( !TCODConsole::isWindowClosed() ) {
+    TCOD_key_t key;
+    TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+    TCODConsole::root->clear();
+    TCODConsole::root->putChar(40,25,'@');
+    TCODConsole::flush();
+}
+return 0;
+}
+
