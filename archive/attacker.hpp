b01e2dc92dc6dc84e51b672e293a3e0316686b3a
xazak <xazak47@gmail.com>
2018-10-31 18:24:13 -0700

    moved tutorial code out into its own directory

--- archive/attacker.hpp
+++
@@ -1,9 +0,0 @@
-class Attacker {
-	public:
-		float power; // damage given
-
-		Attacker(float power);
-		void attack(Actor *owner, Actor *target);
-		void load(TCODZip &zip);
-		void save(TCODZip &zip);
-};
--- archive/persistent.hpp
+++
@@ -1,5 +0,0 @@
-class Persistent {
-	public:
-		virtual void load(TCODZip &zip) = 0;
-		virtual void save(TCODZip &zip) = 0;
-};
